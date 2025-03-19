567d09415eb4f49ada85956edb16001be3d62b50
pophero110 <ojn2014520@gmail.com>
2025-03-19 16:54:04 -0500

    Spawn mob in designated path periodically

---
+++ map.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var mob_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_spawn_timer_timeout():
+	var path_follow_2d = PathFollow2D.new()
+	var mob = mob_scene.instantiate()
+	
+	path_follow_2d.set_script(load("res://path_follow_2d.gd"))
+	
+	$MobPath.add_child(path_follow_2d)
+	path_follow_2d.add_child(mob)
