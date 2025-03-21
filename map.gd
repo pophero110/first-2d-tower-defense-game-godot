bb9f452a062131672847310add13385a9e85a4eb
pophero110 <ojn2014520@gmail.com>
2025-03-21 09:57:35 -0500

    Add tower placement

--- map.gd
+++ map.gd
@@ -1,15 +1,28 @@
 extends Node2D
 
 @export var mob_scene: PackedScene
+@onready var ground_tilemap = $TileMapLayer
+@export var tower_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+	
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		# Try to place tower at mouse position
+		var mouse_pos = get_global_mouse_position()
+		var tile_pos = ground_tilemap.local_to_map(mouse_pos)
+		place_tower(tile_pos)
 
+func place_tower(tile_pos: Vector2i) -> bool:
+		# Instance the tower scene
+		var tower = tower_scene.instantiate()
+		add_child(tower)
+		# Position the tower at the center of the tile
+		var world_pos = ground_tilemap.map_to_local(tile_pos)
+		tower.global_position = world_pos
+		return true
+		
 func _on_spawn_timer_timeout():
 	var path_follow_2d = PathFollow2D.new()
 	var mob = mob_scene.instantiate()
